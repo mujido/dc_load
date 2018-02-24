54db50dfafe50853bf0974da162ae920883003e1
Kurt Stutsman <kstutsman@gmail.com>
2018-02-24 13:58:34 -0600

    Enable ADC with sampling/display 1/s

--- dc_load16.X/main.c
+++ dc_load16.X/main.c
@@ -13,13 +13,52 @@
 #include "system_timer.h"
 #include <string.h>
 #include <libpic30.h>
+#include <stdio.h>
 
 void initIO(void)
 {
-    TRISA = TRISB = 0;
-    LATA = LATB = 0;
-    AD1PCFGL = 0xFFFF;
+    // Set all PORTA pins to output LOW
+    TRISA = 0;
+    LATA = 0;
+
+    // Set B0-RB2,B4-B15 to output LOW. RB3 = ADC input
+    TRISB = 1 << 3;
+    LATB = 0;
+
+    AD1PCFGL = 0xFFFF;      // make all IO ports digital except RB5/AN3
+    AD1PCFGLbits.PCFG5 = 0;
 }
+
+void initADC(void)
+{
+    AD1CON1bits.ADON = 0;       // disable ADC
+    AD1CON1bits.SAMP = 0;       // reset any pending sample
+    AD1CON1bits.ASAM = 0;       // Manual sample start mode
+    AD1CON1bits.SSRC = 0;       // Manual transition between sample and convert
+    AD1CON1bits.FORM = 0b00;    // Integer conversion mode
+    AD1CON1bits.AD12B = 1;      // 12-bit mode
+    AD1CON1bits.ADSIDL = 0;     // Module runs in IDLE mode
+
+    // Sample A only, CH0 only, AVdd/AVss for volt references
+    AD1CON2 = 0;
+
+    AD1CON3bits.ADCS = 0;       // TAD = 1 TCY
+    AD1CON3bits.ADRC = 0;       // ADC clock is FCY
+
+    AD1CHS0bits.CH0SA = 5;      // CH0+ = AD5
+    AD1CHS0bits.CH0NA = 0;      // CH0- = VREFL
+
+    AD1CSSL = 0;                // Disable input scanning
+
+    AD1CON1bits.ADON = 1;       // Enable ADC module
+}
+
+enum ADCState
+{
+    ADC_INACTIVE,
+    ADC_SAMPLING,
+    ADC_CONVERTING
+};
 
 extern const char crlf[];
 const char crlf[2] = { '\r', '\n' };
@@ -31,21 +70,68 @@ int main(void)
     initIO();
     systemTimerInit();
     initSerial1();
+    initADC();
 
     __delay_ms(10);     // Allow time for system to startup
 
     serial1SendLineBlocking("Starting...");
 
-    static const Tick blinkInterval = systemTimerMillisecondsToTicks(500);
+    Tick blinkInterval = systemTimerMillisecondsToTicks(500);
     Tick nextBlink = systemTimerGetCurrent() + blinkInterval;
+
+    Tick adcSampleInterval = systemTimerMillisecondsToTicks(1000);
+    Tick adcSampleTicks = 1;    // sample and conversion time are short
+    Tick adcConvertTicks = 1;   // compared to tick period
+    Tick nextAdcEvent = systemTimerGetCurrent() + adcSampleInterval;
+    uint8_t adcState = ADC_INACTIVE;
+
+    LATBbits.LATB3 = 1;
 
     for (;;)
     {
+        char text[80];
+        uint16_t adcValue;
+
         Tick currentTime = systemTimerGetCurrent();
+
         if (currentTime >= nextBlink)
         {
             LATA ^= 1;
-            nextBlink += blinkInterval;
+            nextBlink = currentTime + blinkInterval;
+        }
+
+        if (currentTime >= nextAdcEvent)
+        {
+            switch (adcState)
+            {
+            case ADC_INACTIVE:
+                AD1CON1bits.SAMP = 1;  // start sampling
+                nextAdcEvent = currentTime + adcSampleTicks;
+                adcState = ADC_SAMPLING;
+                break;
+
+            case ADC_SAMPLING:
+                AD1CON1bits.SAMP = 0;  // start conversion
+                nextAdcEvent = currentTime + adcConvertTicks;
+                adcState = ADC_CONVERTING;
+                break;
+
+            case ADC_CONVERTING:
+                nextAdcEvent = currentTime + adcSampleInterval;
+                adcState = ADC_INACTIVE;
+
+                if (!AD1CON1bits.DONE)
+                {
+                    serial1SendStringBlocking("ADC not done\r\n");
+                    break;
+                }
+
+                adcValue = ADC1BUF0;
+
+                sprintf(text, "%-8lu: %u\r\n", currentTime, adcValue);
+                serial1SendStringBlocking(text);
+                break;
+            }
         }
 
         LineEditStatus leStatus = lineEditReadSerial();
